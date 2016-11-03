25ceefe44b141743fe436e6487f03458c0cf7fc1
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-11-03 15:25:15 +0100

    First commit

---
+++ src/circuit_exporter.cpp
@@ -0,0 +1,52 @@
+#include "circuit_exporter.hpp"
+
+#include <vector>
+
+#include <mvd/mvd3.hpp>
+#include <morpho/morpho_h5_v1.hpp>
+#include <morpho/morpho_tree.hpp>
+
+using namespace std;
+
+mat_points transform(mat_points points,Positions pos, Rotations rot){
+  for(int i = 0; i< points.shape[0]; i = i+1){
+    Point point = point3(points[i][0],points[i][1],points[i][2])
+    hadoken::geometry::rotate<double>(rotations[i],point[j]);
+    for(int j=0; j < 3; ++k){
+      point[j] += translation[j];
+    }
+  }
+  return points;
+}
+
+
+std::vector<morpho_tree> getAllPositions(const std::string & filename){
+  using namespace MVD3;
+  std::string prefix = "./";
+
+  MVD3File file(filename);
+  const Positions positions = file.getPositions();
+  const Rotations rotations = file.getRotations();
+  const std::vector<std::string> morphologies = file.getMorphologies();
+
+  assert(positions.shape[0] == rotations.shape[0] && positions.shape[0] == morphologies.size);
+
+  std::vector<morpho_tree> morpho_trees;
+  for (int i = 0; i < morphologies.size; i = i+1 ){
+    // Getting a morpho_tree for each morphology
+    morpho_reader mread(prefix + morphologies[i]+".h5");
+    morpho_tree tree = mread.create_morpho_tree();
+
+    for (int j=0; j<tree.get_tree_size(); j = j + 1){
+      // Getting every branch of the tree
+      branch br = get_branch(j);
+
+      // Remplace the points matrix with the transformed one.
+      br.set_points(transform(br.get_points(),positions[i],rotations[i]),br.get_distances());
+    }
+
+    morpho_trees.insert(morpho_trees.end(), tree);
+  }
+
+  return morpho_trees;
+}
